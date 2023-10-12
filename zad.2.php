3a0b3da2fc6be3fb30a8e00d9687bbad96e82d2b
Szwarckam <121237750+Szwarckam@users.noreply.github.com>
2023-10-12 20:56:15 +0200

    Update zad.2.php

--- zad.2.php
+++ zad.2.php
@@ -9,9 +9,14 @@ $c = 1;
 $table = "<table>";
 for ($i=1; $i <= 5; $i++) { 
   $table .= "<tr>";
-  for ($j=1; $j <= 8 ; $j++) { 
+  for ($j=1; $j <= 8 ; $j++) {
+    if($i == 1 || $j == 1){ 
     $res =  $j * $c;
+    $table .= "<th> $res </th>";
+    } else {
+      $res =  $j * $c;
     $table .= "<td> $res </td>";
+    }
   }
   $table .= "</tr>";
   // echo "<br>";
